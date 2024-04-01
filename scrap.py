749b7fe2023d75ecfd43edb1a98748d4aef49713
JustBrian <61902485+lbryant-sss@users.noreply.github.com>
2024-04-01 21:55:37 +0300

    Update and rename scarp.py to scrap.py

--- scrap.py
+++ scrap.py
@@ -1,20 +1,14 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
-
-# Set up the Selenium webdriver (make sure you have installed the appropriate browser driver)
-driver = webdriver.Chrome()  # You can change to Firefox or other supported browsers
+driver = webdriver.Chrome()
 
 url = 'https://finance.yahoo.com/lookup'
 
 driver.get(url)
-
-# Extract the HTML content after the page has fully loaded
 html_data = driver.page_source
 
-# Close the browser
 driver.quit()
 
-# Parse the HTML content using BeautifulSoup
 soup = bs(html_data, 'html.parser')
 
 # Find the element you are interested in
